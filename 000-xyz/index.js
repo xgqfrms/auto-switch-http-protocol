de3585e120532273b6c6fbfdc733e06345b842d0
xgqfrms <xgqfrms@outlook.com>
2020-08-25 23:50:49 +0800

    init

---
+++ 000-xyz/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-08-0
+ * @modified
+ *
+ * @description
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+/* // Generator
+const  = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+
+export default ;
+
+export {
+  ,
+};
+ */
